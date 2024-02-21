a6a47914a40e09aa7fa26848cb4ea3b2db26fcde
Ameya Daigavane <ameya.d.98@gmail.com>
2024-02-20 19:48:50 -0500

    Fix launch.

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ counter=2
 for cutoff in 1.0 1.5 2.0 3.0 5.0 10.0
 do
     counter=$((counter+1))
-    CUDA_VISIBLE_DEVICES=$counter python main_qm9.py --nn_cutoff=$((cutoff))  --n_epochs 300 --exp_name "edm_qm9/nn_cutoff=$cutoff" --n_stability_samples 1000 --diffusion_noise_schedule polynomial_2 --diffusion_noise_precision 1e-5 --diffusion_steps 1000 --diffusion_loss_type l2 --batch_size 64 --nf 256 --n_layers 9 --lr 1e-4 --normalize_factors [1,4,10] --test_epochs 10 --ema_decay 0.9999
+    CUDA_VISIBLE_DEVICES=$counter python main_qm9.py --nn_cutoff=$cutoff  --n_epochs 300 --exp_name "edm_qm9/nn_cutoff=$cutoff" --n_stability_samples 1000 --diffusion_noise_schedule polynomial_2 --diffusion_noise_precision 1e-5 --diffusion_steps 1000 --diffusion_loss_type l2 --batch_size 64 --nf 256 --n_layers 9 --lr 1e-4 --normalize_factors [1,4,10] --test_epochs 10 --ema_decay 0.9999
 done
